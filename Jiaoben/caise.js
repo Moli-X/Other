ab96d4e8d77609f497ae07d111f57b238f81dad7
莫离 <327660509@qq.com>
2022-12-03 17:09:03 +0800

    Update caise.js

--- Jiaoben/caise.js
+++ Jiaoben/caise.js
@@ -1,15 +1,5 @@
-// ==UserScript==
-// @name         我要彩色
-// @version      0.1
-// @description  Color back
-// @author       voltachan(https://github.com/voltachan)
-// @match        http*://*/*
-// @grant        GM_addStyle
-// @run-at       document-start
-// @namespace    https://greasyfork.org/users/438767
-// ==/UserScript==
 
 (function() {
     GM_addStyle("* {filter: none !important}");
-    //GM_addStyle("* {-webkit-filter:grayscale(0)! important;-moz-filter:grayscale(0) !important;-ms-filter:grayscale(0) !important;-o-filter:grayscale(0) !important;filter:grayscale(0) !important;filter:none !important;}");
+    GM_addStyle("* {-webkit-filter:grayscale(0)! important;-moz-filter:grayscale(0) !important;-ms-filter:grayscale(0) !important;-o-filter:grayscale(0) !important;filter:grayscale(0) !important;filter:none !important;}");
 })();
